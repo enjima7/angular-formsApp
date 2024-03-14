0655353bfaa0269a2f986d5255a23612d1f13b42
enjima7 <enjima07@gmail.com>
2024-03-14 01:00:53 -0600

    validators added

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 
+const rtx5090 = {
+  name: 'RTX 5090',
+  price: 2500,
+  inStorage: 6,
+}
 @Component({
   templateUrl: './basic-page.component.html',
   styles: [
   ]
 })
-export class BasicPageComponent {
+export class BasicPageComponent implements OnInit{
 
   // public myForm: FormGroup = new FormGroup({
   //   name: new FormControl(''),
@@ -14,14 +19,50 @@ export class BasicPageComponent {
   //   inStorage: new FormControl(0),
   // })
   public myForm: FormGroup = this.fb.group({
-    name: [''],
-    price: [0],
-    inStorage:[0],
+    name: ['', [ Validators.required, Validators.minLength(3) ]],
+    price: [0, [Validators.required, Validators.min(0)]],
+    inStorage:[0, [Validators.required, Validators.min(0)]],
   })
 
   constructor(private fb: FormBuilder){}
 
+
+  ngOnInit(): void {
+    //this.myForm.reset(rtx5090);
+  }
+
+  isValidField(field: string): boolean | null{
+    return this.myForm.controls[field].errors
+    && this.myForm.controls[field].touched;
+  }
+
+  getFieldError(field: string): string | null{
+
+    if(!this.myForm.controls[field] && !this.myForm.controls[field].errors) return null;
+
+    const errors = this.myForm.controls[field].errors || {};
+
+    for (const key of Object.keys(errors)){
+     switch(key){
+      case 'required':
+      return 'Este campo es requerido';
+
+      case 'minlength':
+        return `Minimo ${errors['minlength'].requiredLength} caracteres`
+     }
+    }
+
+    return null;
+
+  }
+
   onSave():void{
+
+    if(this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+    this.myForm.reset();
     console.log('my form valiu')
   }
 }
